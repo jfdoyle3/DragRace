ed3b667a59e3d34d83025b615bfa5a7add7789bb
jfdoyle3 <36973560+jfdoyle3@users.noreply.github.com>
2021-09-10 04:00:43 -0400

    a car can have an engine selected

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DragStrip.race();
+      //  DragStrip.race();
+        PitArea.carSelection();
 
     }
 }
--- src/com/company/game/PitArea.java
+++ src/com/company/game/PitArea.java
@@ -2,6 +2,7 @@ package com.company.game;
 
 import com.company.ui.UI;
 import com.company.vehicles.Car;
+import com.company.vehicles.engine.Electric;
 import com.company.vehicles.engine.Engine;
 import com.company.vehicles.engine.InternalCombustion;
 
@@ -10,20 +11,27 @@ import java.util.List;
 
 public class PitArea {
 
-    public static void xPitArea() {
+    public static void carSelection() {
         List<Car> cars = new ArrayList<>();
-        InternalCombustion gasPowered = null;
-        Car car = null;
-//        int selectCar= UI.selectCar();
-//        int selectEngine= UI.selectEngine();
-//        if(selectEngine==1)
-        gasPowered = new InternalCombustion("V8", 760, 190, 100);
-        //     if(selectCar==2)
-        car = new Car("GT500", 4, 4171, gasPowered, 16);
+        List<Engine> engines=new ArrayList<>();
+
+
+
+        int selectCar = UI.selectCar();
+        int selectEngine = UI.selectEngine();
+
+        InternalCombustion    gasPoweredV8 = new InternalCombustion("V8", 760, 190, 100);
+        InternalCombustion    gasPoweredV12 = new InternalCombustion("V12", 760, 190, 100);
+        Electric electricPowered=new Electric(100,150,100,8);
+        engines.add(gasPoweredV8);
+        engines.add(gasPoweredV12);
+        engines.add(electricPowered);
+        Car car = new Car("GT500", 4, 4171, engines.get(selectEngine-1), 16);
+        car.generatePassengers(4);
         cars.add(car);
 
-        // car.generatePassengers(4);
-        System.out.println(car.toString());
+
+        System.out.println(selectCar+" "+selectEngine+"\n"+car);
     }
 
 
